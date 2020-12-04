04643bd2246c14fad1de6f066d465f580524f0c0
RajivGyawali <73691571+RajivGyawali@users.noreply.github.com>
2020-12-04 13:36:12 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,54 @@
+var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
+var packageBody,ground
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+
+}
+
+function setup() {
+	createCanvas(1200, 600);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+
+
+	Engine.run(engine);
+
+	paperobject = new paper(100,230,15)
+	ground = new Ground(600,550,1200,10)
+	bottom = new DustBin(1075,480,50,50)
+	rod1 = new Side(1020,480,10,100)
+	rod2 = new Side(1125,480,10,100)
+	                                                            
+
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background("white");
+  paperobject.display();
+  ground.display();
+  bottom.display();
+  rod1.display();
+  rod2.display();
+  drawSprites();
+}
+
+
+function keyPressed() {
+	if (keyCode === UP_ARROW){
+	Matter.Body.applyForce(paperobject.Body,paperobject.Body.position,{x:48,y:-48});
+	}
+}
+
+
+
